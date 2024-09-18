b207914551f334aedb4d9c005a631767d4fdf9ba
StormSynn <anstoine56@gmail.com>
2024-09-18 01:15:14 -0400

    Étape 10: fini debut partie héritage

--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -25,35 +25,10 @@ public class JeuGladiateurs {
     // <editor-fold defaultstate="collapsed" desc="Affichage pré-combat">
     // TODO : Afficher les infos de chaque personnage
     
-    System.out.println(Bob.getNom());
-    System.out.println(" Attaque : " + Bob.getValeurMaxAttaque());
-    System.out.println(" Defense : " + Bob.getValeurDefense());
-    System.out.println(" Points de vie : " + Bob.getPointsDeVie());
-    System.out.println(" Initiative : " + Bob.getInitiative());
-        if (Bob.getPointsDeVie() <= 0 ) 
-        {
-            System.out.println("Statut: Mort");
-        }
-        else  
-        {
-            System.out.println(" Statut: Vivant \n");
-        }
-    System.out.println(Igor.getNom());
-    System.out.println(" Attaque : " + Igor.getValeurMaxAttaque());
-    System.out.println(" Defense : " + Igor.getValeurDefense());
-    System.out.println(" Points de vie : " + Igor.getPointsDeVie());
-    System.out.println(" Initiative : " + Igor.getInitiative());
-        if (Igor.getPointsDeVie() <= 0 ) 
-        {
-            System.out.println(" Statut: Mort");
-        }
-        else  
-        {
-            System.out.println("Statut: Vivant \n");
-        }
     // TODO : Afficher le message du début du combat
     affichage.afficherDebutCombat();
     
+  
     
     // </editor-fold>
     // **************************************************************************
@@ -61,6 +36,30 @@ public class JeuGladiateurs {
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Mécanique de combat">
     // TODO : La boucle contenant les étapes du combat
+      do
+        {            
+          tour.afficheTour();
+          for (int i = 0; i < 101; i++) 
+           {
+                if (Bob.getInitiative() == i)
+                {
+                    Bob.frapperPersonnage(Igor);
+                    System.out.println();
+                }
+                if (Igor.getInitiative()== i) 
+                {
+                    Igor.frapperPersonnage(Bob);
+                }
+           }
+           affichage.afficherSeparateurInfosPerso();       
+           Bob.afficherInfosPersonnage();
+           Igor.afficherInfosPersonnage();
+           Bob.setNewInitiativeRandom();
+           Igor.setNewInitiativeRandom();
+           tour.augmenteTour();
+           affichage.afficherSeparateurDeTour();
+        }while (Bob.getPointsDeVie() != 0 && Igor.getPointsDeVie() != 0);
+       affichage.afficheVictoire(Bob, Igor);
     // TODO : Après la boucle, afficher le résultat du combat
     // </editor-fold>
     }
